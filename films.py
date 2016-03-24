5399f219fa6e9dbe6927e1478847f55c5cc3a53d
Miguh Ruiz <miguhruiz@gmail.com>
2016-03-24 15:12:15 +0100

    Added new films according to geek things

--- films.py
+++ films.py
@@ -1,14 +1,14 @@
 import info
 import fresh_tomatoes
 
-toy_story = info.Film("Toy Story", "http://www.gstatic.com/tv/thumb/movieposters/17420/p17420_p_v8_ab.jpg", "KYz2wyBy3kc")
+option1 = info.Film("Steve Jobs", "http://pics.filmaffinity.com/Steve_Jobs-647763435-large.jpg", "aEr6K1bwIVs")
 # print(toy_story.title)
 
-avatar = info.Film("Avatar", "http://t0.gstatic.com/images?q=tbn:ANd9GcQCfmvrE4fMo2cd8esc7mDZPtFSJThAujddMPkRtti1_ij6u-jp", "kbA9TfGphOI")
+option2 = info.Film("The Matrix", "http://pics.filmaffinity.com/The_Matrix-155050517-large.jpg", "m8e-FF8MsqU")
 # print(avatar.title)
 # avatar.show_trailer()
 
-school_of_rock = info.Film("School Of Rock", "http://cps-static.rovicorp.com/3/JPG_400/MI0000/398/MI0000398846.jpg?partner=allrovi.com", "XCwy6lW5Ixc")
+option3 = info.Film("The Social Network", "http://pics.filmaffinity.com/The_Social_Network-460155430-large.jpg", "lB95KLmpLR4")
 
-movies = [toy_story, avatar, school_of_rock]
+movies = [option1, option2, option3]
 fresh_tomatoes.open_movies_page(movies)
