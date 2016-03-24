50b274fa720c646125b6f973c552abd2455066ad
Miguh Ruiz <miguhruiz@gmail.com>
2016-03-24 15:31:56 +0100

    Organised the class file. Renamed it too.

--- definitions.py
+++ definitions.py
@@ -1,9 +1,6 @@
-import webbrowser
+# Defintion of the only class that the "app" needs:
 class Film():
     def __init__(self, name, poster, videoid):
         self.title = name
         self.poster_url = poster
         self.video_id = videoid
-
-    def show_trailer(self):
-        webbrowser.open(self.video_url)
--- films.py
+++ films.py
@@ -1,14 +1,14 @@
-import info
+import definitions
 import fresh_tomatoes
 
-option1 = info.Film("Steve Jobs", "http://pics.filmaffinity.com/Steve_Jobs-647763435-large.jpg", "aEr6K1bwIVs")
+option1 = definitions.Film("Steve Jobs", "http://pics.filmaffinity.com/Steve_Jobs-647763435-large.jpg", "aEr6K1bwIVs")
 # print(toy_story.title)
 
-option2 = info.Film("The Matrix", "http://pics.filmaffinity.com/The_Matrix-155050517-large.jpg", "m8e-FF8MsqU")
+option2 = definitions.Film("The Matrix", "http://pics.filmaffinity.com/The_Matrix-155050517-large.jpg", "m8e-FF8MsqU")
 # print(avatar.title)
 # avatar.show_trailer()
 
-option3 = info.Film("The Social Network", "http://pics.filmaffinity.com/The_Social_Network-460155430-large.jpg", "lB95KLmpLR4")
+option3 = definitions.Film("The Social Network", "http://pics.filmaffinity.com/The_Social_Network-460155430-large.jpg", "lB95KLmpLR4")
 
 movies = [option1, option2, option3]
 fresh_tomatoes.open_movies_page(movies)
